6c2b506f817a83667dc46dfd09d0227515ca8c4c
evY000 <000evan001@gmail.com>
2023-12-11 13:15:24 -0500

    ASDFGHJKL;./

--- api-project/main.js
+++ api-project/main.js
@@ -1,12 +1,33 @@
 import './style.css';
-const URL = "https://api.disneyapi.dev/character";
-async function GetCharacter(){
+const DOMSelectors = {
+  Thing : document.getElementById("card"),
+}
+const Link = "https://api.disneyapi.dev/character";
+const Mouse = "https://api.disneyapi.dev/character?name=Mickey%20Mouse";
+ async function GetCharacter(){
   try {
-    const response = await fetch(URL);
+    const response = await fetch(Link);
     const stuff = await response.json();
-   //const answer = stuff.info.data.films.filter((name)=> console.log(name.info.data.films.includes("Hercules")));  
-    console.log(stuff)}
+    stuff.forEach((card)=> {
+      const FirstName = card.films
+      const LastName = card.name
+      const Image = card.url
+    const card2 = ` <div class = "Card">
+    <h1> ${FirstName} </h1>
+    <h2> ${LastName} </h2> 
+    <img class ="imgs" src="${Image}"</div>`
+    DOMSelectors.Thing.insertAdjacentHTML('afterend', card2);
+    })
+  }
     catch (error) {
       console.log(error);}}
-GetCharacter(URL)
+GetCharacter(Link) 
+async function TheMouse(){
+  try{
+  const GetMouse = await fetch(Mouse);
+  const Mousey = await GetMouse.json();
+  console.log(Mousey)}
+  catch (error){
+    console.log(error);}}
+TheMouse(Mouse)
 
