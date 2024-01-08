05eb919749fee693b33d6806bfa28da6dad9a151
evY000 <000evan001@gmail.com>
2024-01-08 07:32:43 -0500

    api went down :sob:

--- api-project/main.js
+++ api-project/main.js
@@ -4,29 +4,42 @@ const DOMSelectors = {
   Christmas: document.querySelector('#christmas'),
   body: document.querySelector('#body'),
   Halloween: document.querySelector('#halloween'),
-  Input : document.querySelector('#input'),
+  Input : document.querySelector('input'),
+  Submit : document.querySelector("submit"),
+  form: document.getElementById("form"),
 }
- 
+function something(){
+  DOMSelectors.form.addEventListener("submit",function(event){
+      event.preventDefault()
+      let user = DOMSelectors.Input.value;
+      console.log(user)
+})}
+something()
 async function GetInput(){
   try {
-     let NameSearch = `https://api.disneyapi.dev/character?name=${DOMSelectors.Input}`
+     /* let NameSearch = `https://api.disneyapi.dev/character?name=${DOMSelectors.Input}` 
+     console.log(NameSearch) */
      let Link = "https://api.disneyapi.dev/character?page=1"
      // Set Link &pageSize=10000000 when all characters
      const response = await fetch(Link);
     const stuff = await response.json();  
-    stuff.data.forEach((card)=> {
+
+    let user = DOMSelectors.Input.value;
+    stuff.data.forEach((card)=> {card
       const FirstName = card.films
       const LastName = card.name
       const Image = card.imageUrl
-      const cards = `<div class = "Card">
+      const cards = `<div tabindex ="7" class =  "Card">
       <h1> ${FirstName} </h1>
       <h2> ${LastName} </h2> 
       <img class ="imgs" src="${Image}" </div>`
-      DOMSelectors.Thing.insertAdjacentHTML('beforeend', cards);   
+      DOMSelectors.Thing.insertAdjacentHTML('beforeend', cards); 
 })}
   catch(error) {
     console.log('error');}} 
  GetInput()  
+
+
 DOMSelectors.Christmas.addEventListener("click", function(){
   if (document.body.classList.contains("normal")){
      document.body.classList.add("christmas")
@@ -94,3 +107,5 @@ const Mouse = "https://api.disneyapi.dev/character?name=Mickey%20Mouse"; */
     console.log(error);}}
 TheMouse(Mouse)
  */
+
+ 
